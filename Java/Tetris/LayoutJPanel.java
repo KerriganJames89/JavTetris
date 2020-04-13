518dacfc163a215da5728f495daab4fed9e3ec78
KerriganJames89 <63562380+KerriganJames89@users.noreply.github.com>
2020-04-13 00:12:15 -0400

    Add files via upload

--- Java/Tetris/LayoutJPanel.java
+++ Java/Tetris/LayoutJPanel.java
@@ -7,10 +7,11 @@ import javax.swing.JPanel;
 import java.awt.Component;
 import java.awt.event.*;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
 
-  private Timer t = new Timer(500, this);
+  private Timer t = new Timer(250, this);
   private double velX = 2, velY = 2;
   private int x = 0, y = 0;
   private int delay = 0;
@@ -20,7 +21,10 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
   private Random random = new Random(); 
   private int newShape = 1;
   
-  //Tetris board: 10 x 22
+  //gracePeriod gives the player time to move their shape when onto of another shape
+  private int gracePeriod = 1;
+  
+  //Tetris board: 10 x 22; only 10 x 20 is drawn on the panel since shapes are created outside of player vision
   private int arr[][] = new int[10][22];
 
   
@@ -51,43 +55,26 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
         g2d.setColor(Color.BLACK);
         g2d.draw(coordinates);
         
+        //need to make a random function in here that selects a shape randomly
         if(newShape == 1)
         {
           //square starting values
           x = 120;
           y = 0;
-          
+        
           newShape = 0;
         }
         
-        g2d.fillRect(x, y - 60, 60, 60);
         
-        //coordinate testing
-        //System.out.println("x value is:" + x);
-        //System.out.println("y value is:" + y);
-        //System.out.println();
+        //coordinates for a newly made square;
+        //need to make a function for each shape coordinates and their rotation positions
+        g2d.setColor(Color.RED);
+        g2d.fillRect(x + 1, y - 29, 29, 29);
+        g2d.fillRect(x + 1, y - 59, 29, 29);
+        g2d.fillRect(x + 31, y - 29, 29, 29);
+        g2d.fillRect(x + 31, y - 59, 29, 29);
         
-        
-    //specific to the 4x4 square block piece that detects collision    
-    if(y  == 600 
-    || arr[(x) / 30][(y + 60) / 30] == 1
-    || arr[(x + 30) / 30][(y + 60) / 30] == 1)
-    {
-      arr[((x)/30)][(y/30)] = 1;
-      arr[((x + 30)/30)][(y/30)] = 1;
-      arr[((x)/30)][((y + 30)/30)] = 1;
-      arr[((x + 30)/30)][((y + 30)/30)] = 1;
-       
-      
-      x = 0;
-      y = 0;
-      
-      newShape = 1;
-      
-      //print test
-      printBoard();
-      
-    }
+
       
      //Checks tetris board for all block locations and fills them
     for(int i = 0; i < 10; i++)
@@ -96,7 +83,8 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
       {
         if(arr[i][j] == 1)
         {
-          g2d.fillRect((i * 30), (j * 30) - 60, 30, 30);
+          g2d.setColor(Color.RED);
+          g2d.fillRect((i * 30) + 1, (j * 30) - 59, 29, 29);
         }
       }
     }
@@ -115,15 +103,58 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
         System.out.print(arr[j][i] + " ");
       }
     }
+    
+    //coordinate testing
+    //System.out.println("x value is:" + x);
+    //System.out.println("y value is:" + y);
+    //System.out.println();
+    
+    
     System.out.println();
     System.out.println("--------------------------");
   }
  
-  public void actionPerformed(ActionEvent e){
+  public void actionPerformed(ActionEvent e)
+  {
     
+    //check if square is ontop of another square;
+    //we need a function for ALL shapes/rotations here  
+    if(y  == 600 
+    || arr[(x) / 30][(y + 60) / 30] == 1
+    || arr[(x + 30) / 30][(y + 60) / 30] == 1)
+    {
     
+      if(gracePeriod > 0)
+      {
+        gracePeriod = gracePeriod - 1;
+        return;
+      }
+      
+      else
+      {
+        //sets square coordinates to ascii 2d array; 
+        //we need a function for ALL shapes/rotations here
+        arr[((x)/30)][(y/30)] = 1;
+        arr[((x + 30)/30)][(y/30)] = 1;
+        arr[((x)/30)][((y + 30)/30)] = 1;
+        arr[((x + 30)/30)][((y + 30)/30)] = 1;
+         
+        
+        x = 0;
+        y = 0;
+        
+        newShape = 1;
+        gracePeriod = 1;
+        
+        //print test
+        printBoard();
+        repaint();
+        return;
+      }
+    }
+    
+    gracePeriod = 1;
     y += 30;
-    
     repaint();
   }
   
@@ -132,13 +163,14 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
       timeCurrent = System.currentTimeMillis();
       timeCheck = timeCurrent - timePrevious;
   
-    if (timeCheck < 0 || timeCheck > 100) 
+    if (timeCheck < 0 || timeCheck > 50) 
     {
       timePrevious = timeCurrent;
       
       int key = e.getKeyCode();
       
-      //collision only works with this specific shape
+      //collision only works with this specific square shape
+      //we need a function for each key to check for collision for ALL shapes/rotations
       if (key == KeyEvent.VK_LEFT) 
       {
         if(x != 0 
@@ -165,12 +197,18 @@ public class LayoutJPanel extends JPanel implements ActionListener, KeyListener{
       }
     
       else if (key == KeyEvent.VK_DOWN) 
-      {
-        y += 30;
+      { 
+      if(y  != 600 
+        && arr[(x) / 30][(y + 60) / 30] != 1
+        && arr[(x + 30) / 30][(y + 60) / 30] != 1)
+        {
+          y += 30;
+        }   
+        
+        gracePeriod = 0; 
       }
-      
-      repaint();
     }
+    repaint();
   }
   
   public void keyReleased(KeyEvent e) 
